b073a3f22f075181fb3527ff3280a13cb6b4b5a8
Bielfo1 <biielfo@gmail.com>
2025-04-24 22:00:42 -0300

    Add files via upload

---
+++ Aula 5/array.js
@@ -0,0 +1,18 @@
+// Inicie um array vazio chamado usuários
+let usuarios = []
+console.log(usuarios)
+
+// Adicione no final deste array um elemento com valor -> Gabriel
+usuarios.push("Gabriel")
+
+// Imprima no terminal o valor de "usuarios"
+console.log(usuarios)
+
+// Inicie um novo array chamado "lista_clientes" com os elementos: Gabriel, Matheus, Leonardo, Jair e João do Caminhão
+let lista_clientes = ["Gabriel", "Matheus", "Leonardo", "Jair","João do Caminhão"]
+console.log(lista_clientes[2])
+
+//Depois de visualizar estes dados , atualize o terceiro elemento para “Leozin de cria
+
+lista_clientes.splice (2, 1, "Leozin de Cria")
+console.log(lista_clientes)
